7c80c3e25e2d9af0e7913016b4027609e6683d54
MukaSanches <ssanches18@icloud.com>
2026-09-15 11:35:03 -0300

    chore: expose v0.2 autonomous index API

--- src/suzano_aberta/__init__.py
+++ src/suzano_aberta/__init__.py
@@ -1,12 +1,16 @@
 from .core import Suzano, explain
-from .models import IntegrityFinding, IntegrityReport, PublicRecord, SourceRef
+from .models import IntegrityFinding, IntegrityReport, PublicRecord, RefreshReport, SourceRef
+from .snapshot import LATEST_SNAPSHOT_URL, SnapshotError
 
 __all__ = [
     "IntegrityFinding",
     "IntegrityReport",
+    "LATEST_SNAPSHOT_URL",
     "PublicRecord",
+    "RefreshReport",
+    "SnapshotError",
     "SourceRef",
     "Suzano",
     "explain",
 ]
-__version__ = "0.1.1"
+__version__ = "0.2.0"
